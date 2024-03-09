543ea091afd9699ffb7b6de4bcf780dd8d7d7a1d
rodneyrjl <rodneyjeanlouis@gmail.com>
2024-03-09 12:36:59 -0500

    Run from package

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -25,6 +25,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        // Added RUN_FROM_PACKAGE
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
