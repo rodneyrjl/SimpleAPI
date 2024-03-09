6f904a931c66903b11e490b88cc0f3babd2baa43
rodneyrjl <rodneyjeanlouis@gmail.com>
2024-03-09 12:44:41 -0500

    Remove Run from package

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        // Added RUN_FROM_PACKAGE
+        // Remoce RUN_FROM_PACKAGE from Portal
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
